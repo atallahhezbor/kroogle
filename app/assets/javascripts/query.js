934545437ae5df29b4d9f1590e6ab5a7367972ab
Atallah <atallahhezbor@gmail.com>
2016-03-26 17:13:06 -0400

    Makes conversation box react

--- app/assets/javascripts/query.js
+++ app/assets/javascripts/query.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
 	$('#ask-form').submit(function(event){
-		// event.preventDefault();		
-		// console.log("form submitted");
+		event.preventDefault();
 		var query = $('#query').val();
+		$('#response').append('<div class="sent message"><span>' + query + '</span></div>')
+		
 		$.ajax('welcome/get_response', {
 				method: 'POST',				
 				data: {
 					query: query
 				},
 				success: function(response) {
-					alert(response.text)
+					$('#response').append('<div class="received message">' + response.text + '</div>')
 				},
 				failure: function(response) {
 					alert('failure');
